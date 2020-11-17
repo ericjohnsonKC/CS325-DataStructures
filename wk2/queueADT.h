febc33400026c2bd39eb18abac5037d0ee0febca
Eric Johnson <ericjohnson.kc@gmail.com>
2020-11-17 16:31:42 -0600

    Added file queueADT.h.

---
+++ wk2/queueADT.h
@@ -0,0 +1,58 @@
+/************************************
+ *  Author: Eric Johnson
+ *  Date:   11/17/2020
+ * 
+ *  Grantham University
+ *  CS325 Data Structures
+ *  Week 2: Queues
+ * 
+ *  This assignment is to create a queue data structure
+ *  given the instructions.
+ * 
+ *  This file will define an abstract class for a queue.
+ * ***********************************/
+
+template <typename T>
+class queueAbstractClass
+{
+    public:
+        virtual bool isEmptyQueue() const = 0;
+            //Function to determine whether queue is empty.
+            //Postcondition: Returns true if the queue is empty,
+            //  otherwise returns false.
+
+        virtual bool isFullQueue() const = 0;
+            //Function to determine whethere queue is full.
+            //Postcondition: Returns true if the queue is full,
+            //  otherwise returns false.
+
+        virtual void initializeQueue() = 0;
+            //Function to initialize the queue to an empty state.
+            //Postcondition: The queue is empty.
+
+        virtual T front() const = 0;
+            //Function to return the first element of the queue.
+            //Precondition: The queue exists and is not empty.
+            //Postcondition: If the queue is empty, the program 
+            //  terminates; otherwise, the first element of the 
+            //  queue is returned.
+
+        virtual T back() const = 0; 
+            //Function to return the last element of the queue.
+            //Precondition: The queue exists and is not empty.
+            //Postcondition: If the queue is empty, the progream
+            //  terminates; otherwise, the last element of the queue
+            //  is returned.
+
+        virtual void addQueue(const T& queueElement) = 0;
+            //Function to add queueElement to the queue.
+            //Precondition: The queue exists and is not full.
+            //Postcondition: The queue is changed and queueElement
+            //  is added to the queue.
+
+        virtual void deleteQueue() = 0;
+            //Function to remove the first element of the queue.
+            //Precondition: The queue exists and is not empty.
+            //Postcondition: The queue is changed and the first element
+            //  is removed from the queue.
+};
